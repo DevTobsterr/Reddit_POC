a6f802c00f719ad8d5818c8947bc64d8cd36eeb0
Toby Killen <itoby24@gmail.com>
2021-08-05 18:59:31 +0100

    Added Esclations to the front end.

--- reddit/urls.py
+++ reddit/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.application_home, name="application_home"),
     path('submission/', views.application_submisson_search, name="application_submisson_search"),
+    path('submission/esclations/', views.applcation_submission_esclation, name="application_submisson_esclation"),
     path('submission/results', views.application_submisson_results, name="application_submisson_results"),
 
     
--- reddit/views.py
+++ reddit/views.py
@@ -1,10 +1,13 @@
 from re import sub
 from django.db.models.expressions import F
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from praw.reddit import Submission
 from .forms import SubredditForm
 from reddit import forms
 from django.core.exceptions import ObjectDoesNotExist
+import datetime
+from django.urls import reverse
 
 from reddit.models import Reddit_Submission
 
@@ -12,6 +15,8 @@ from reddit.models import Reddit_Submission
 # Reddit Imports
 import praw
 import os
+# End of Reddit Imports
+
 
 
 
@@ -26,6 +31,9 @@ def application_submisson_search(request):
     form = SubredditForm()
     return render(request, "reddit/application_submission_search.html", {'form': form })
 
+def applcation_submission_esclation(request):
+    return render(request, "reddit/application_submission_esclation.html")
+
 
 
 
@@ -37,32 +45,68 @@ def application_submisson_results(request):
         form = SubredditForm(request.POST)
 
         if form.is_valid():
-            user_subreddit = form.cleaned_data['subreddit']
-            
             reddit = praw.Reddit(
             client_id="DPn6cOtSZYZJug",
             client_secret="WQj8aPewT55hn3JQcBO0M8f1o-AqIg",
             user_agent="itoby24"
             )
 
+
+            user_subreddit = form.cleaned_data['subreddit']
+            user_search_type = form.cleaned_data['sort_type']
+            number_of_results = form.cleaned_data['result_count']
+
             subreddit = reddit.subreddit(user_subreddit)
+            if user_search_type == 'Hot':
+                for submission in subreddit.hot(limit=number_of_results):
+                    results = Reddit_Submission()
+                    results.submission_id = submission.id
+                    results.submission_time_stamp = unix_time_converter(submission.created_utc)
+                    results.submission_author = submission.author
+                    results.submission_title = submission.title
+                    results.submission_url = submission.url
+                    results.submission_score = submission.score
+                    results.save()
+                return redirect(reverse(application_submisson_results))
 
-            for submission in subreddit.new(limit=1):
-                reddit_submission = Reddit_Submission(
-                    submission_id = submission.id,
-                    submission_time_stamp = submission.created_utc,
-                    submission_title = submission.title,
-                    submission_score = submission.score,
-                    submission_author = submission.author,
-                    submission_url = submission.url,
-                )
-                try: 
-                    checking_for_duplicate = Reddit_Submission.objects.get(submission_id=submission.id)
-                except ObjectDoesNotExist:
-                    reddit_submission.save()
+            elif user_search_type == "New":
+                for submission in subreddit.new(limit=number_of_results):
+                    results = Reddit_Submission()
+                    results.submission_id = submission.id
+                    results.submission_time_stamp = unix_time_converter(submission.created_utc)
+                    results.submission_author = submission.author
+                    results.submission_title = submission.title
+                    results.submission_url = submission.url
+                    results.submission_score = submission.score
+                    results.save()
+                return redirect(reverse(application_submisson_results))
 
+            elif user_search_type == "Top":
+                for submission in subreddit.top(limit=number_of_results):
+                    results = Reddit_Submission()
+                    results.submission_id = submission.id
+                    results.submission_time_stamp = unix_time_converter(submission.created_utc)
+                    results.submission_author = submission.author
+                    results.submission_title = submission.title
+                    results.submission_url = submission.url
+                    results.submission_score = submission.score
+                    results.save()
+                return redirect(reverse(application_submisson_results))
+
+            elif user_search_type == "Rising":
+                for submission in subreddit.rising(limit=number_of_results):
+                    results = Reddit_Submission()
+                    results.submission_id = submission.id
+                    results.submission_time_stamp = unix_time_converter(submission.created_utc)
+                    results.submission_author = submission.author
+                    results.submission_title = submission.title
+                    results.submission_url = submission.url
+                    results.submission_score = submission.score
+                    results.save()
+                return redirect(reverse(application_submisson_results))
+            else:
+                pass 
             submission_results = Reddit_Submission.objects.all()
-            
             return render(request, 'reddit/application_submission_results.html', {'submission_results': submission_results})
         else:
             return render(request, "reddit/application_error_404.html")
@@ -102,4 +146,11 @@ class Reddit:
 
 
 
-   
+
+
+
+
+
+def unix_time_converter(unix_time):
+    real_time = datetime.datetime.fromtimestamp(unix_time).strftime('%Y-%m-%d %H:%M:%S')
+    return real_time
