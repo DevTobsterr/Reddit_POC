e518c62436557a001e883df8b2bad48a52f6dd20
Toby Killen <itoby24@gmail.com>
2021-08-07 16:33:38 +0100

    Font-end Change and Support for Esclations

--- reddit/models.py
+++ reddit/models.py
@@ -11,4 +11,14 @@ class Reddit_Submission(models.Model):
     submission_author = models.CharField(max_length=32)
     submission_url = models.CharField(max_length=32)
 
+
+# class Reddit_Submission_Esclation(models.Model):
+#     submission_id = models.CharField(max_length=32, unique=True)
+#     submission_subreddit = models.CharField(max_length=21, null=True)
+#     submission_time_stamp = models.CharField(max_length=32)
+#     submission_title = models.CharField(max_length=32)
+#     submission_score = models.CharField(max_length=32)
+#     submission_author = models.CharField(max_length=32)
+#     submission_url = models.CharField(max_length=32)
+
     
--- reddit/urls.py
+++ reddit/urls.py
@@ -4,9 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.application_home, name="application_home"),
+
+
+
+    # SUBMISSION RELEATED URLS
     path('submission/results/', views.application_submission_results, name="application_submission_results"),
     path('submission/results/delete/<submission_id>', views.application_delete_submission, name="application_delete_submission"),
-
     path('submission/search/', views.application_submission_search, name="application_submission_search"),
     path('submission/search/query/', views.application_submission_query, name="application_submisson_search"),
     path('submission/esclations/', views.applcation_submission_esclation, name="application_submisson_esclation"),
